a6198bd2471a2145ffff7f43af8b70fb0a5ae897
Daniel Veiga <148176584+DatedMind@users.noreply.github.com>
2025-09-07 01:25:19 -0300

    Update java007Statement.java

--- src/Study/java007Statement/java007Statement.java
+++ src/Study/java007Statement/java007Statement.java
@@ -1,10 +1,24 @@
 package Study.java007Statement;
 
+import java.util.Random;
+
 public class java007Statement {
 
     public static void main(String[] args){
 
-        //
+        Random random = new Random();
+        // if statement = ele funciona bloqueando entradas de infos que você define como false
+
+        int age = random.nextInt(100)+1;
+        System.out.println("Idade: "+age);
+
+        if(age <= 59 || age >= 18){
+            System.out.println("Você é um adulto");
+        }else if(age >= 60){
+            System.out.println("Você é um idoso");
+        }else{
+            System.out.println("Você é uma criança");
+        }
 
     }
 
