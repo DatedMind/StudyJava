501eb0bfe3c9a6a8db7f326de4a234c4ad36389c
Daniel Veiga <148176584+DatedMind@users.noreply.github.com>
2025-09-07 22:31:50 -0300

    new

--- src/Study/java009LogicOperators/java009LogicOperators.java
+++ src/Study/java009LogicOperators/java009LogicOperators.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class java009LogicOperators {
 
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
         // logical operators == &&(AND), ||(OR), !(NOT)
 
         boolean x = true;
@@ -21,8 +21,21 @@ public class java009LogicOperators {
             System.out.println("Nooooo");
         }
 
-        
+        // Aperte q ou Q e sair
 
+        while(true) {
+            System.out.println("Digite 'q' ou 'Q' para sair do jogo");
+
+            String response = scanner.next();
+            if(!response.equals("q") && !response.equals("Q")){
+
+                System.out.println("Você continua a jogar 'pew, pew'");
+
+            } else {
+
+                System.out.println("Obrigado por jogar!");
+                return;
+            }
+        }
     }
-
 }
---
+++ src/Study/java010/java010.java
@@ -0,0 +1,11 @@
+package Study.java010;
+
+import javax.swing.JOptionPane;
+
+public class java010 {
+
+    public static void main(String[] args){
+        String str = JOptionPane.showInputDialog("Escreva o que quiser");
+        JOptionPane.showMessageDialog(null, str);
+    }
+}
