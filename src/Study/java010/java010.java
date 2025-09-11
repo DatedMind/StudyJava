a9d9e4658242b22411a1dab5a67993b8af2d32c5
Daniel Veiga <148176584+DatedMind@users.noreply.github.com>
2025-09-10 23:08:40 -0300

    Update java010.java

--- src/Study/java010/java010.java
+++ src/Study/java010/java010.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 public class java010 {
 
     public static void main(String[] args){
-        String str = JOptionPane.showInputDialog("Escreva o que quiser");
-        JOptionPane.showMessageDialog(null, str);
     }
 }
+
